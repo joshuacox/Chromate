9c32018e9d9c4d23787a912820a30288cfdf906f
Josh Cox <josh@webhosting.coop>
2017-07-22 09:26:00 -0500

    arch

--- start.sh
+++ start.sh
@@ -4,24 +4,21 @@ MYCWD=$(pwd)
 TZ=${TZ:-America/Chicago}
 export CHROME_BIN=google-chrome
 
-rm /etc/localtime
-cd /etc; ln -s /usr/share/zoneinfo/$TZ localtime
 cd $MYCWD
 
 if [ -s /LINK ]; then
-  echo "cat /LINKS |xargs -n1 -I{} nice -n $NICENESS $CHROME_BIN {}"
-  echo "cat /LINKS |xargs -n1 -I{} google-chrome {}"
-  #cat $LINK |xargs -n1 -I{} nice -n $NICENESS $CHROME_BIN {}
+  nice -n $NICENESS $CHROME_BIN \
+  --restore-last-session \
+  --user-data-dir=/data \
+  --no-sandbox \
+  --host-resolver-rules="$MAP" \
+  --proxy-server="$PROXY" \
+  `cat /LINK`
+else
   nice -n $NICENESS $CHROME_BIN \
   --restore-last-session \
   --user-data-dir=/data \
   --host-resolver-rules="$MAP" \
   --proxy-server="$PROXY" \
-  `cat /LINK`
-else
-  nice -n $NICENESS google-chrome \
-  --restore-last-session \
-  --user-data-dir=/data \
-  --proxy-server="$PROXY" \
-  --host-resolver-rules="$MAP"
+  'http://bokbot.org/'
 fi
